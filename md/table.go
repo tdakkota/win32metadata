c87f065f7f187909bdcb3eac38bfca67f1e98821
tdakkota <tanc13@yandex.ru>
2021-07-16 16:11:25 +0300

    feat(md): add Table and TableHeader decoders

---
+++ md/table.go
@@ -0,0 +1,76 @@
+package md
+
+// TableType is a metadata table type.
+//
+// See II.22.1 Metadata validation rules.
+type TableType int
+
+const (
+	Module                 TableType = 0x00
+	TypeRef                TableType = 0x01
+	TypeDef                TableType = 0x02
+	Field                  TableType = 0x04
+	MethodDef              TableType = 0x06
+	Param                  TableType = 0x08
+	InterfaceImpl          TableType = 0x09
+	MemberRef              TableType = 0x0a
+	Constant               TableType = 0x0b
+	CustomAttribute        TableType = 0x0c
+	FieldMarshal           TableType = 0x0d
+	DeclSecurity           TableType = 0x0e
+	ClassLayout            TableType = 0x0f
+	FieldLayout            TableType = 0x10
+	StandaloneSig          TableType = 0x11
+	EventMap               TableType = 0x12
+	Event                  TableType = 0x14
+	PropertyMap            TableType = 0x15
+	Property               TableType = 0x17
+	MethodSemantics        TableType = 0x18
+	MethodImpl             TableType = 0x19
+	ModuleRef              TableType = 0x1a
+	TypeSpec               TableType = 0x1b
+	ImplMap                TableType = 0x1c
+	FieldRva               TableType = 0x1d
+	Assembly               TableType = 0x20
+	AssemblyProcessor      TableType = 0x21
+	AssemblyOs             TableType = 0x22
+	AssemblyRef            TableType = 0x23
+	AssemblyRefProcessor   TableType = 0x24
+	AssemblyRefOs          TableType = 0x25
+	File                   TableType = 0x26
+	ExportedType           TableType = 0x27
+	ManifestResource       TableType = 0x28
+	NestedClass            TableType = 0x29
+	GenericParam           TableType = 0x2a
+	MethodSpec             TableType = 0x2b
+	GenericParamConstraint TableType = 0x2c
+)
+
+// Column represents metadata table column sizes.
+type Column struct {
+	// Offset from row start.
+	Offset uint32
+	// Size of column in bytes.
+	Size uint32
+}
+
+// Table represents metadata table header.
+type Table struct {
+	Type    TableType
+	Rows    uint32
+	RowSize uint32
+	Columns [6]Column
+}
+
+// SetRowType sets rows sizes.
+// NB: Zero size means that column is not present.
+func (t *Table) SetRowType(sizes [6]uint32) {
+	t.RowSize = 0
+	for i, column := range sizes {
+		t.Columns[i] = Column{
+			Offset: t.RowSize,
+			Size:   column,
+		}
+		t.RowSize += column
+	}
+}
---
+++ md/table_header.go
@@ -0,0 +1,82 @@
+package md
+
+import (
+	"io"
+)
+
+type RowCount struct {
+	Tag   int
+	Count uint32
+}
+
+// TablesHeader is a representation of II.24.2.6 #~ stream.
+type TablesHeader struct {
+	Reserved     [4]byte
+	MajorVersion uint8
+	MinorVersion uint8
+	HeapSizes    uint8
+	Reserved2    [1]byte
+	Valid        uint64
+	Sorted       uint64
+	Rows         []RowCount
+}
+
+// StringIndexSize returns size of index of "#String" heap.
+func (h TablesHeader) StringIndexSize() int {
+	if h.HeapSizes&1 == 1 {
+		return 4
+	}
+	return 2
+}
+
+// BlobIndexSize returns size of index of "#Blob" heap.
+func (h TablesHeader) BlobIndexSize() int {
+	if (h.HeapSizes>>2)&1 == 1 {
+		return 4
+	}
+	return 2
+}
+
+// GUIDIndexSize returns size of index of "#GUID" heap.
+func (h TablesHeader) GUIDIndexSize() int {
+	if (h.HeapSizes>>1)&1 == 1 {
+		return 4
+	}
+	return 2
+}
+
+// Decode decodes TablesHeader from stream.
+func (h *TablesHeader) Decode(r io.Reader) error {
+	rr := reader{r: r}
+
+	if !rr.Read(&h.Reserved) ||
+		!rr.Read(&h.MajorVersion) ||
+		!rr.Read(&h.MinorVersion) ||
+		!rr.Read(&h.HeapSizes) ||
+		!rr.Read(&h.Reserved2) ||
+		!rr.Read(&h.Valid) ||
+		!rr.Read(&h.Sorted) {
+		return rr.Err()
+	}
+
+	// II.24.2.6 #~ stream
+	// ...
+	// The Valid field is a 64-bit bitvector that has a specific bit set for each table that is stored in the stream;
+	// the mapping of tables to indexes is given at the start of §II.22.
+	var row uint32
+	for i := 0; i < 64; i++ {
+		if h.Valid>>i&1 == 0 {
+			continue
+		}
+
+		if !rr.Read(&row) {
+			return rr.Err()
+		}
+		h.Rows = append(h.Rows, RowCount{
+			Tag:   i,
+			Count: row,
+		})
+	}
+
+	return nil
+}
